7d0a403ae764ebd439439faf14c7b6962c61277a
Pavol Porubsky <paulooze@gmail.com>
2021-09-17 15:01:42 +0200

    refactor coordinates set fn

--- src/components/Diagram/DiagramNode/portGenerator.js
+++ src/components/Diagram/DiagramNode/portGenerator.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import Port from '../Port/Port';
 
--- src/components/Diagram/NodesCanvas/updateNodeCoordinates.js
+++ src/components/Diagram/NodesCanvas/updateNodeCoordinates.js
@@ -1,18 +1,16 @@
-import findIndex from 'lodash.findindex';
-
 /**
  * Given a node id, an pair of new coordinates and the nodes array, clones the nodes array and update the node
  * having that id with the new coordinates, then returns the cloned array.
  */
-const updateNodeCoordinates = (nodeId, coordinates, nodes) => {
-  const index = findIndex(nodes, ['id', nodeId]);
-
-  if (index > -1 && !nodes[index].disableDrag) {
-    // eslint-disable-next-line no-param-reassign
-    nodes[index].coordinates = coordinates;
+const updateNodeCoordinates = (nodeId, newCoordinates, nodes) => nodes.map((node) => {
+  if (node.id === nodeId) {
+    const { coordinates, ...rest } = node;
+    return {
+      ...rest,
+      coordinates: newCoordinates,
+    };
   }
-
-  return nodes;
-};
+  return node;
+});
 
 export default updateNodeCoordinates;
